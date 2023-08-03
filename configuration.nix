abf70891fa67c679657f2ce85aacf23cac3422d5
山下 <hello@yamashit.ax>
2023-08-04 06:51:57 +0900

    Add more settings

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,8 @@
     casks = [ "google-japanese-ime" ];
   }; 
 
+  security.pam.enableSudoTouchIdAuth = true;
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
@@ -41,6 +43,20 @@
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";    
   };
+
+  # Darwin settings
+  system.defaults.NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
+  system.defaults.NSGlobalDomain.AppleMetricUnits = 1;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.AppleTemperatureUnit = "Celsius";
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  
+  system.defaults.trackpad.ActuationStrength = 0; # silent clicking
+  system.defaults.trackpad.FirstClickThreshold = 0; # light clicking
+  system.defaults.trackpad.SecondClickThreshold = 0; # light force touch
+
+  system.keyboard.enableKeyMapping = true;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
