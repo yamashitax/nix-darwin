71d027e0e9ca7c08a4ef90cfe2bfd9f329bb25e1
山下 <hello@yamashit.ax>
2023-08-21 19:15:17 +0900

    Add color schemes to wezterm

--- home/common.nix
+++ home/common.nix
@@ -59,8 +59,39 @@ in {
       return {
         font = wezterm.font("JetBrains Mono"),
         hide_tab_bar_if_only_one_tab = true,
+        color_scheme = "test",
       }
       '';
+      colorSchemes.test = rec {
+        ansi = [
+          "#000000"
+          "#ff3333"
+          "#86b200"
+          "#f19618"
+          "#41a6d9"
+          "#f07078"
+          "#4cbe99"
+          "#ffffff"
+        ];
+        brights = [
+          "#323232"
+          "#ff6565"
+          "#b8e532"
+          "#ffc849"
+          "#73d7ff"
+          "#ffa3aa"
+          "#4cbe99"
+          "#ffffff"
+        ];
+        foreground = "#5b6673";
+        background = "#fafafa";
+        cursor_bg = "#ff6900";
+        cursor_border = "#ff6900";
+        selection_bg = "#f0ede4";
+        selection_fg = "#fafafa";
+        scrollbar_thumb = "#333333";
+        split = "#444444";
+      };
       enableZshIntegration = true;
     };
   };
