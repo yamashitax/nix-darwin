b758a7742dd1ace16b1db91ba6cf1e57c0770e76
山下 <git@yamashit.ax>
2024-06-03 11:18:55 +0200

    chore: adjust git settings

--- home/yamashita/home.nix
+++ home/yamashita/home.nix
@@ -8,11 +8,12 @@
   imports = [ ../common.nix ];
 
   programs = {
+    git.userEmail = "hello@yamashit.ax";
+    git.userName = "山下";
+
     jujutsu.settings.user = {
       name = "山下";
       email = "git@yamashit.ax";
     };
-
-    git.userEmail = "hello@yamashit.ax";
   };
 }
